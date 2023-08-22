264886e84c0b80e89f9dd4a9ecca1e9ddaa3cba0
Richard StJohn <stjohn.rick@icloud.com>
2023-08-22 19:37:42 -0400

    refactor

---
+++ local_deploy.sh
@@ -0,0 +1,5 @@
+cd /Users/bigdata/repos/nifi-influxdb2-bundle/nifi-influxdb2-nar-assembly/target/
+tar xvf nifi-influxdb2-nars-0.1.0-SNAPSHOT.tar.gz
+cd /Users/bigdata/repos/nifi-influxdb2-bundle/nifi-influxdb2-nar-assembly/target/nifi-influxdb2-nars-0.1.0-SNAPSHOT
+
+mv *.nar /Users/bigdata/Downloads/applications/nifi-1.20.0/lib
